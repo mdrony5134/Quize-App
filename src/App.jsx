0e836d9aaa12eac6781f90318ea0063aa2c9afba
mdrony5134 <rony830388@gmail.com>
2024-01-13 21:58:59 +0600

    Starting project quize app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+ 
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='app' style={{backgroundImage:url("./ques1.png")}}></div>
+
+    </>
+  )
+}
+
+export default App
